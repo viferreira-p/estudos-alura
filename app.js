8ef653ecacfe3bb56160491bd971ae33e02f714a
Vitória Ferreira <pferreiravit@gmail.com>
2024-10-24 14:23:36 -0300

    adicionando if e else

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas Vindas ao Jogo do Número Secreto');
-let secretnumber = 5;
-prompt ('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+let tentativa = prompt ('Escolha um número entre 1 e 10');
+
+if(tentativa == numeroSecreto){
+    console.log('Acertou miseravel (5)');
+}
