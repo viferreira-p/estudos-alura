1a21be998da51f6af34d64039473561650db9875
Vitória Ferreira <pferreiravit@gmail.com>
2024-10-28 08:33:47 -0300

    adicionando while

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-alert('Boas Vindas ao Jogo do Número Secreto');
+alert("Boas Vindas ao Jogo do Número Secreto");
 let numeroSecreto = 5;
-let tentativa = prompt ('Escolha um número entre 1 e 10');
+let tentativa;
 
-if(tentativa == numeroSecreto){
-    alert(`Você acertou o número secreto! ${numeroSecreto}`);
-} else {
-    alert('Você errou ):');
+// enquanto a tentativa não for igual ao número secreto
+while (tentativa != numeroSecreto) {
+  tentativa = prompt("Escolha um número entre 1 e 10");
+  if (tentativa == numeroSecreto) {
+    alert(`Parabéns, acertou o número secreto ${numeroSecreto}`);
+  } else {
+    if (tentativa > numeroSecreto) {
+      alert(`O número secreto é menor que ${tentativa}`);
+    } else {
+      alert(`O número secreto é maior que ${tentativa}`);
+    }
+  }
 }
