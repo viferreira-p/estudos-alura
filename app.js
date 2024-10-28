01be8203780626ed208542c53f2415b340f0328a
Vitória Ferreira <pferreiravit@gmail.com>
2024-10-28 08:41:56 -0300

    adicionando incrementação ao código

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 alert("Boas Vindas ao Jogo do Número Secreto");
 let numeroSecreto = 5;
 let tentativa;
+let numeroTentativas = 1;
+
 
 // enquanto a tentativa não for igual ao número secreto
 while (tentativa != numeroSecreto) {
   tentativa = prompt("Escolha um número entre 1 e 10");
+  // se a tentativa for igual, maior ou menor ao número secreto
   if (tentativa == numeroSecreto) {
-    alert(`Parabéns, acertou o número secreto ${numeroSecreto}`);
+    alert(`Parabéns, acertou o número secreto ${numeroSecreto} com ${numeroTentativas} tentativas!`);
   } else {
     if (tentativa > numeroSecreto) {
       alert(`O número secreto é menor que ${tentativa}`);
     } else {
       alert(`O número secreto é maior que ${tentativa}`);
     }
+    numeroTentativas++;
   }
 }
