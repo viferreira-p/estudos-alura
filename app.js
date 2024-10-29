c9c46947f9cfe3a8bc9a3439622e75c7aad1995a
Vitória Ferreira <pferreiravit@gmail.com>
2024-10-29 10:55:19 -0300

    finalizando o curso

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert("Boas Vindas ao Jogo do Número Secreto");
-let numeroSecreto = parseInt(Math.random() * 100 + 1)
+let numeroMaximo = 1000
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1)
 let tentativa;
 let numeroTentativas = 1;
 
 
 // enquanto a tentativa não for igual ao número secreto
 while (tentativa != numeroSecreto) {
-  tentativa = prompt("Escolha um número entre 1 e 100");
+  tentativa = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
   // se a tentativa for igual, maior ou menor ao número secreto
   if (tentativa == numeroSecreto) {
     break;
